6ee891e4468efa13568578e2c9360c1b06f5066a
ThiagoBibiano <thiagobibiano@ymail.com>
2025-11-23 14:04:25 -0300

    feat: desenvolvimento de interpretador de dados para o pytorch

--- solvers/rl/__init__.py
+++ solvers/rl/__init__.py
@@ -1,8 +1,20 @@
 """Camada de RL (Reinforcement Learning) para Sudoku."""
 
 from .env import RewardConfig, SudokuGymEnv
+from .data import (
+    SudokuCSVDataset,
+    build_dataloader,
+    encode_puzzle_one_hot,
+    givens_mask,
+    puzzle_str_to_grid,
+)
 
 __all__ = [
     "RewardConfig",
     "SudokuGymEnv",
+    "SudokuCSVDataset",
+    "build_dataloader",
+    "encode_puzzle_one_hot",
+    "givens_mask",
+    "puzzle_str_to_grid",
 ]
